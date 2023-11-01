1ed4e406a9dd0373cf6431034be1280f53f7ff43
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-01 17:00:06 +0200

    0.2.0 - SCREENS SETUP 📱

---
+++ app/src/main/java/com/sergiu/libihb/presentation/screens/contact/ContactUsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.libihb.screens.contact
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun ContactUsScreen(navController: NavController) {
+    Text(text = "ContactUsScreen")
+}
