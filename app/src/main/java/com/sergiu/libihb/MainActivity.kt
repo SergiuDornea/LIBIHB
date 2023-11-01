f8b481072eb7f3ebb411e6d57a06f9b598b3778c
SergiuDornea <88648596+SergiuDornea@users.noreply.github.com>
2023-11-01 17:35:48 +0200

    0.3.0 - NAV SETUP 🗺

--- app/src/main/java/com/sergiu/libihb/MainActivity.kt
+++ app/src/main/java/com/sergiu/libihb/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.libihb.navigation.LIBIHBNavigation
 import com.sergiu.libihb.ui.theme.LIBIHBTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LIBIHBApp()
+
                 }
             }
         }
@@ -30,17 +32,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LIBIHBTheme {
-        Greeting("Android")
+fun LIBIHBApp(){
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        // set the navigation as the center of everything
+        LIBIHBNavigation()
     }
 }
+
+
